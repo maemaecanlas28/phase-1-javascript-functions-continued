79cf6cd9cf398086a9f8d0c389f1eb9a82fa93b0
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-28 15:04:52 -0700

    commit tree 8f3053d0dc241a1ff6587c2f4e06f250a4c3ecb6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (event = "roller-skate") {
+    return `This Saturday, I want to ${event}!`;
+}
+
+function mondayWork (task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective (asterisk) {
+    return function innerfunction (compliment = "special") {
+        return `You are ${asterisk}${compliment}${asterisk}!`;
+    }
+}
